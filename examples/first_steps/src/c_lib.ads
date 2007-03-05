cf2ff2d3b63a7626ed042ea5bd18e9c2d2d0f091
Cyrille Comar <comar@adacore.com>
2007-03-05 17:59:27 +0000

    examples from the ug
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gprmake@42924 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ examples/first_steps/src/c_lib.ads
@@ -0,0 +1,5 @@
+package C_lib is
+   procedure Do_Something;
+   pragma Import (C, Do_Something);
+end C_Lib;
+
---
+++ examples/first_steps/src/c_main.c
@@ -0,0 +1,6 @@
+extern void do_it_in_ada(void);
+void main (void) {
+  do_it_in_ada (void);
+}
+
+
