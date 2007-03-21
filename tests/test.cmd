e2dcbd0f06cef1da8a8e05b002e20505b3fd4612
Emmanuel Briot <briot@adacore.com>
2007-03-21 15:50:05 +0000

    Provide dummy local compiler for the testsuite, simulating gcc and gnatmake Update baselines accordingly (and also to match changes that were done in  the knowledge base apparently)
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gprmake@43253 936e1b1b-40f2-da11-902a-00137254ae57

--- tests/test.cmd
+++ tests/test.cmd
@@ -5,7 +5,7 @@ test_config () {
   shift
   switches="$@"
   pwd=`pwd`
-  ../gprconfig -o output.gpr -batch $switches
+  ../gprconfig -o output.gpr -batch $switches | sed -e '/Creating configuration file/,$d'
   sed -e "s,$pwd,.," output.gpr > output2.gpr
   diff $expected output2.gpr >/dev/null
   if test $? != 0 ; then
@@ -16,21 +16,23 @@ test_config () {
 }
 
 
+pwd=`pwd`
+
 ## Testing lynxos
-test_config test1.out -config 'GNAT,/usr/foo/bin/,5.04,Ada,x86-lynxos'
+test_config test1.out -config "GNAT,$pwd/bin/,5.04,Ada,x86-lynxos"
 
 ## Testing windows
-test_config test2.out -config 'GNAT,/usr/foo/bin/,5.04,Ada,pentium-mingw32'
+test_config test2.out -config "GNAT,$pwd/bin/,5.04,Ada,pentium-mingw32"
 
 ## Testing tru64
-test_config test3.out -config 'GNAT,/usr/foo/bin/,5.04,Ada,alphaev56-dec-osf5.1'
+test_config test3.out -config "GNAT,$pwd/bin/,5.04,Ada,alphaev56-dec-osf5.1"
 
 ## Testing vms
-test_config test4.out -config 'GNAT,/usr/foo/bin/,5.04,Ada,alpha-openvms'
+test_config test4.out -config "GNAT,$pwd/bin/,5.04,Ada,alpha-openvms"
 
 ## Testing linux
-test_config test5.out -config 'GNAT,/usr/foo/bin/,5.04,Ada,i686-pc-linux-gnu'
-test_config test5.out -config 'GNAT,/usr/foo/bin/,5.04,Ada,i585-suse-linux'
+test_config test5.out -config "GNAT,$pwd/bin/,5.04,Ada,i686-pc-linux-gnu"
+test_config test5.out -config "GNAT,$pwd/bin/,5.04,Ada,i585-suse-linux"
 
 
 
