7c9cb965d77e8eaa394f5f59bad2c1223136704a
Tristan Gingold <gingold@adacore.com>
2007-12-14 11:03:43 +0000

    --show-targets now displays all targets available.
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gprbuild@120132 936e1b1b-40f2-da11-902a-00137254ae57

--- src/gprconfig-main.adb
+++ src/gprconfig-main.adb
@@ -171,8 +171,7 @@ procedure GprConfig.Main is
       Put_Line (" --target=target (" & Sdefault.Hostname & " by default)");
       Put_Line
         ("            Select specified target or ""all"" for any target.");
-      Put_Line (" --show-targets : List all compiler targets that are known");
-      Put_Line ("            to match the one specified by --target.");
+      Put_Line (" --show-targets : List all compiler targets available.");
       Put_Line (" -o file  : Name and directory of the output file.");
       Put_Line ("            default is " & To_String (Output_File));
       Put_Line (" --db dir : Parse dir as an additional knowledge base.");
@@ -867,6 +866,9 @@ begin
                   Selected_Target := To_Unbounded_String (Parameter);
                   Output_File := To_Unbounded_String (Parameter & ".cgpr");
                end if;
+            elsif Full_Switch = "-show-targets" then
+               --  By default, display all targets available
+               Selected_Target := Null_Unbounded_String;
             end if;
 
          when 'q' =>
