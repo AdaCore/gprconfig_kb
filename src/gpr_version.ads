d0b84e7e11efe3fc727b76dfd2ba3dd06c618a71
Olivier Ramonat <ramonat@adacore.com>
2009-10-09 14:49:17 +0000

    Bump gprbuild version.
    Part of IA08-003
    
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gprbuild@150553 936e1b1b-40f2-da11-902a-00137254ae57

--- src/gpr_version.ads
+++ src/gpr_version.ads
@@ -36,7 +36,7 @@
 
 package GPR_Version is
 
-   Gpr_Version : constant String := "1.4.0w";
+   Gpr_Version : constant String := "1.5.0w";
    --  Static string identifying this version
 
    function Gpr_Version_String return String;
