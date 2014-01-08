75e617e26e7d993888f675b9caa500e1c6e30ba9
Pascal Obry <obry@adacore.com>
2014-01-08 18:52:31 +0000

    Fix -r option description.
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gprbuild@221704 936e1b1b-40f2-da11-902a-00137254ae57

--- src/gprinstall-main.adb
+++ src/gprinstall-main.adb
@@ -5,7 +5,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---          Copyright (C) 2012-2013, Free Software Foundation, Inc.         --
+--          Copyright (C) 2012-2014, Free Software Foundation, Inc.         --
 --                                                                          --
 -- This is free software;  you can redistribute it  and/or modify it  under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
@@ -667,7 +667,7 @@ procedure Gprinstall.Main is
 
          --  Line for -r
 
-         Write_Line ("  -r       Recursive (default except when using -c)");
+         Write_Line ("  -r       Recursive");
 
          --  Line for -a
 
