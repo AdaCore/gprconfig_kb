5d57ee4a015912ffccbfefad0746d7bda00eac22
Vincent Celier <celier@adacore.com>
2015-01-14 17:50:58 +0000

    Parse knowledge base or runtime only for selected compilers
    Fixes O113-051
    
    
    
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gprbuild@235408 936e1b1b-40f2-da11-902a-00137254ae57

--- src/gprconfig-main.adb
+++ src/gprconfig-main.adb
@@ -5,7 +5,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---         Copyright (C) 2006-2014, Free Software Foundation, Inc.          --
+--         Copyright (C) 2006-2015, Free Software Foundation, Inc.          --
 --                                                                          --
 -- This is free software;  you can redistribute it  and/or modify it  under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
@@ -584,7 +584,9 @@ begin
          while Compiler_Lists.Has_Element (Cursor) loop
             Comp := Compiler_Lists.Element (Cursor);
 
-            if Runtime_Dir_Of (Comp) /= No_Name then
+            if Is_Selected (Comp.all) and then
+              Runtime_Dir_Of (Comp) /= No_Name
+            then
                declare
                   RTS : constant String :=
                           Get_Name_String (Runtime_Dir_Of (Comp));
