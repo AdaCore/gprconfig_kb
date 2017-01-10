814792b2fbb5eedb694a9f2628c3ca0074ec6df9
Bob Duff <duff@adacore.com>
2017-01-10 09:02:41 -0500

    gprclean: Allow for Lib_Dir.Value to be No_Name
    * gpr/src/gpr-nmsc.adb:
    (Check_Library_Attributes): Set Library_Directory_Present to
    False if Lib_Dir.Value = No_Name.
    
    Fixes Q107-004

--- gpr/src/gpr-nmsc.adb
+++ gpr/src/gpr-nmsc.adb
@@ -2,7 +2,7 @@
 --                                                                          --
 --                           GPR PROJECT MANAGER                            --
 --                                                                          --
---          Copyright (C) 2000-2016, Free Software Foundation, Inc.         --
+--          Copyright (C) 2000-2017, Free Software Foundation, Inc.         --
 --                                                                          --
 -- This library is free software;  you can redistribute it and/or modify it --
 -- under terms of the  GNU General Public License  as published by the Free --
@@ -3753,7 +3753,7 @@ package body GPR.Nmsc is
    --  Start of processing for Check_Library_Attributes
 
    begin
-      Library_Directory_Present := Lib_Dir.Value /= Empty_String;
+      Library_Directory_Present := Lib_Dir.Value not in No_Name | Empty_String;
 
       --  Special case of extending project
 
