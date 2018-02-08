e837c2b5238a79327443d7c5f8d9eba2791e4b6d
Fedor Rybin <frybin@adacore.com>
2018-02-08 18:59:55 +0300

    R129-029 quit parsing source file on illegal character
    When parsing source file to detect whether it is a separate
    or not, do not continue parsing when stumbling upon an
    illegal character. This helps avoiding hangups on binary
    files passed on as sources by accident.
    
    Change-Id: Id8592ff090030b5ceb66337c31933c0003846c88

--- gpr/src/gpr-err-scanner.adb
+++ gpr/src/gpr-err-scanner.adb
@@ -2,7 +2,7 @@
 --                                                                          --
 --                           GPR PROJECT MANAGER                            --
 --                                                                          --
---       Copyright (C) 2015-2017, Free Software Foundation, Inc.           --
+--       Copyright (C) 2015-2018, Free Software Foundation, Inc.           --
 --                                                                          --
 -- This library is free software;  you can redistribute it and/or modify it --
 -- under terms of the  GNU General Public License  as published by the Free --
@@ -1649,6 +1649,10 @@ package body Scanner is
               EM  | FS  | GS  | RS  | US  | DEL
          =>
             Error_Illegal_Character;
+            if Language_For_Scanner = Ada then
+               Token := Tok_EOF;
+               return;
+            end if;
 
          --  Invalid graphic characters
 
